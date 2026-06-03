727f65e075b7f1bebfbb0ebda72e318ecc7e2d93
JagmeetSingh9 <jagmeetsingh999991@gmail.com>
2026-06-03 20:49:35 +0530

    fix api interceptor and remove withCredentials

--- frontend/src/features/auth/services/authapi.js
+++ frontend/src/features/auth/services/authapi.js
@@ -1,69 +1,57 @@
 import axios from "axios"
 
-
 const api = axios.create({
-    baseURL: "http://localhost:3000",
-    withCredentials: true
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+})
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token")
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
 })
 
 export async function register({ username, email, password }) {
-
+    // eslint-disable-next-line no-useless-catch
     try {
-        const response = await api.post('/api/auth/register', {
-            username, email, password
-        })
-
+        const response = await 
+        api.post('/api/auth/register', { username, email, password })
+        localStorage.setItem("token", response.data.token)
         return response.data
-
     } catch (err) {
-
-        console.log(err)
-
+        throw err
     }
-
 }
 
 export async function login({ email, password }) {
-
+    // eslint-disable-next-line no-useless-catch
     try {
-
-        const response = await api.post("/api/auth/login", {
-            email, password
-        })
-
+        const response = await api.post("/api/auth/login", { email, password })
+        localStorage.setItem("token", response.data.token)
         return response.data
-
     } catch (err) {
-        console.log(err)
+        throw err
     }
-
 }
 
 export async function logout() {
+    // eslint-disable-next-line no-useless-catch
     try {
-
         const response = await api.get("/api/auth/logout")
-
+        localStorage.removeItem("token")
         return response.data
-
     } catch (err) {
-        console.log(err)    
+        throw err
     }
 }
 
 export async function getMe() {
-
+    // eslint-disable-next-line no-useless-catch
     try {
-
         const response = await api.get("/api/auth/me")
-
         return response.data
-
     } catch (e) {
-        console.log(e)
         throw e
     }
-
 }
-
-
